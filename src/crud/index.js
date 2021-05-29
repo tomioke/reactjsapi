9cf691428a1f26a45ee263be865975883eec412e
tomioke <tomiirvan@gmail.com>
2021-05-29 11:36:08 +0700

    null

---
+++ src/crud/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Crud extends Component {
+  render() {
+    return (
+      <div>
+        {/* Test */}
+        <h2>Success!!</h2>
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+// import './index.css';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Crud from './crud';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Crud />
   </React.StrictMode>,
   document.getElementById('root')
 );
